fe86296a5ce894b15df7bdf7d46a4a5f74a32907
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-18 10:27:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_primjer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>()
+            {
+                "C# Tutorials",
+                "VB .NET Tutorials",
+                "Learn C++",
+                "HVC Tutorials",
+                "Java"
+            };
+            var result = from s  in stringList
+                         where s.Contains("Tutorials")
+                         select s;
+            foreach(var str in result)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
